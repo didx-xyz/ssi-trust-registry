768657444542fd1f95dd90036604ea170ae2a6f7
Jakub Koci <jakub.koci@gmail.com>
2023-06-15 18:46:16 +0200

    Use async and error handler middlewares

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,30 @@
 import express from 'express'
-import { Server } from 'http'
+import { Server } from 'node:http'
+import fs from 'node:fs/promises'
+import { asyncHandler, errorHandler } from './middleware'
 
 export async function startServer(): Promise<Server> {
   return new Promise((resolve, reject) => {
     const port = 3000
     const app = express()
 
+    app.set('json spaces', 2)
+
     app.get('/health', (req, res) => {
       res.status(200).send('OK')
     })
+
+    app.get(
+      '/registry',
+      asyncHandler(async (req, res) => {
+        const registryContent = await fs.readFile('./src/registry.json', {
+          encoding: 'utf8',
+        })
+        res.status(200).json(JSON.parse(registryContent))
+      })
+    )
+
+    app.use(errorHandler)
 
     const server = app.listen(port, (error?: Error) => {
       if (error) {
