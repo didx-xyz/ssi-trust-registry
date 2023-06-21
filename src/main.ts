8cc6bdfa975cb4f1dfd136ac622129ad7ef4c0a8
Jakub Koci <jakub.koci@gmail.com>
2023-06-21 14:50:39 +0200

    Add signals handlers to shutdown

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,15 @@ import { startServer } from './server'
 
 function main() {
   startServer()
+  process.on('SIGINT', shutdownGracefully())
+  process.on('SIGTERM', shutdownGracefully())
+}
+
+function shutdownGracefully() {
+  return async (signal: string) => {
+    console.log(`Received ${signal}. Stopping the service...`)
+    process.exit()
+  }
 }
 
 main()
