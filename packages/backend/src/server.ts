47c0cf8c2c3d6d68708f40b1fac8ed346820f348
Niall Shaw <niall.shaw@absa.africa>
2023-10-30 11:13:41 +0100

    chore: disable submissions api in production

--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -72,6 +72,7 @@ export function startServer(
 
     apiRouter.get(
       '/submissions',
+      disableInProduction,
       asyncHandler(async (req, res) => {
         logger.info('Reading the registry from the file.')
         const submissions = await context.submissionService.getAllSubmissions()
@@ -81,6 +82,7 @@ export function startServer(
 
     apiRouter.post(
       '/submissions/:invitationId',
+      disableInProduction,
       asyncHandler(async (req, res) => {
         const payload = { ...req.body, invitationId: req.params.invitationId }
         logger.info(`Processing submission:`, payload)
