74078a65ca8c197bcc5fbf6919d0a0a8752c94ff
Niall Shaw <niall.shaw@absa.africa>
2023-11-14 16:30:41 +0100

    feat: invite form successful

--- packages/frontend/src/app/invite/components/InviteForm.tsx
+++ packages/frontend/src/app/invite/components/InviteForm.tsx
@@ -6,6 +6,7 @@ import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 
 import * as z from 'zod'
+import Link from 'next/link'
 
 interface Inputs {
   emailAddress: string
@@ -17,22 +18,24 @@ export function InviteForm({ authToken }: { authToken?: string }) {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isSubmitting, isSubmitSuccessful },
+    getValues,
+    reset,
   } = useForm<Inputs>({
     resolver: zodResolver(schema),
   })
-  const [isLoading, setIsLoading] = useState(false)
-
   async function onSubmit(data: Inputs) {
-    setIsLoading(true)
-    try {
-      await invite(data, authToken)
-    } catch {}
-    setIsLoading(false)
+    await invite(data, authToken)
   }
 
-  return (
+  return !isSubmitSuccessful ? (
     <div>
+      <p className="text-2xl font-bold leading-normal pb-2">Invite a company</p>
+      <p className="text-sm leading-normal">
+        Enter email address of the company&apos;s representative,
+        <br />
+        that you want to invite.
+      </p>
       <TextInput
         type="text"
         name="emailAddress"
@@ -47,9 +50,45 @@ export function InviteForm({ authToken }: { authToken?: string }) {
           className="btn btn-primary px-12 text-white normal-case"
           onClick={handleSubmit(onSubmit)}
         >
-          {isLoading && <span className="loading loading-spinner"></span>}
+          {isSubmitting && <span className="loading loading-spinner"></span>}
           Send invitation
         </button>
+      </div>
+    </div>
+  ) : (
+    <div className="flex flex-col justify-center items-center text-primary">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="124"
+        height="124"
+        viewBox="0 0 124 124"
+        fill="none"
+      >
+        <circle cx="62" cy="62" r="62" fill="#2BD18F" />
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M94.8218 44.165C96.3876 45.7234 96.3935 48.2561 94.835 49.8218L59.0032 85.8218C58.2528 86.5757 57.2331 86.9997 56.1694 87C55.1057 87.0003 54.0857 86.577 53.3348 85.8236L35.1667 67.5929C33.6073 66.0281 33.6117 63.4955 35.1764 61.936C36.7412 60.3766 39.2739 60.381 40.8333 61.9458L56.1664 77.3316L89.165 44.1782C90.7234 42.6125 93.2561 42.6065 94.8218 44.165Z"
+          fill="white"
+        />
+      </svg>
+      <div className="py-8 ">
+        <p className="text-2xl font-bold">Invitation sent</p>
+        <p className="text-sm pt-2">
+          Invitation was successfully sent to {getValues('emailAddress')}
+        </p>
+      </div>
+      <div className="flex gap-4">
+        <button className="btn bg-medium  normal-case" onClick={() => reset()}>
+          {isSubmitting && <span className="loading loading-spinner"></span>}
+          Send another invitation
+        </button>
+        <Link
+          href="/"
+          className="btn bg-accent h-12 min-w-[192px] text-white normal-case hover:bg-accentHover"
+        >
+          Back to Trusted Entities
+        </Link>
       </div>
     </div>
   )
--- packages/frontend/src/app/invite/page.tsx
+++ packages/frontend/src/app/invite/page.tsx
@@ -7,20 +7,11 @@ export default function InvitePage() {
 
   return (
     <main
-      style={{ color: '#2D3E47' }}
       className="flex min-h-screen flex-col w-full items-center"
     >
       <NavigationBreadcrumbs rootHref="/" rootName="Trusted Entities" />
 
       <div className="card rounded-2xl p-16 bg-white text-center w-1/2 min-w-[40rem] max-w-4xl">
-        <p className="text-2xl font-bold leading-normal pb-2">
-          Invite a company
-        </p>
-        <p className="text-sm leading-normal">
-          Enter email address of the company&apos;s representative,
-          <br />
-          that you want to invite.
-        </p>
         <InviteForm authToken={authToken} />
       </div>
     </main>
