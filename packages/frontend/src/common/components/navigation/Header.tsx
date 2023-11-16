f63aa9db76339d02f651672dc04495cd0ad17258
Niall Shaw <niall.shaw@absa.africa>
2023-11-16 10:25:17 +0100

    fix: absolute path to logo

--- packages/frontend/src/common/components/navigation/Header.tsx
+++ packages/frontend/src/common/components/navigation/Header.tsx
@@ -8,7 +8,7 @@ export function Header() {
     <div className="sticky top-0 z-10 bg-white h-20 shadow-lg flex justify-between px-6 items-center">
       <Link href="/" className="flex gap-2">
         <Image
-          src="images/logo.svg"
+          src="/images/logo.svg"
           width={116}
           height={24}
           alt="Trust Registry"
