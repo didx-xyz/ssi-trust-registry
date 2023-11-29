036bed07682e72011266ff34de437e6f775a2ab8
Andrii Uhryn <andrii.uhryn@absa.africa>
2023-11-29 16:27:36 +0100

    fix: unauthorized page margins

--- packages/frontend/src/common/components/auth/Unauthorized.tsx
+++ packages/frontend/src/common/components/auth/Unauthorized.tsx
@@ -12,9 +12,11 @@ export function Unauthorized() {
     <PageContainer>
       <div className="flex justify-center mt-14">
         <div className="card card-compact bg-white shadow-xl p-16 w-full max-w-[585px]">
-          <div className="card-body items-center !p-0 gap-y-4">
-            <Text2xlBold>Unauthorized</Text2xlBold>
-            <TextSm>You need to be authorized to access this page</TextSm>
+          <div className="card-body items-center !p-0 gap-y-8">
+            <div className="flex flex-col items-center gap-y-2">
+              <Text2xlBold>Unauthorized</Text2xlBold>
+              <TextSm>You need to be authorized to access this page</TextSm>
+            </div>
             <Button
               title="Login as Admin"
               onClick={() => {
