4a2110628ca04198a3f69507000f460b41763a80
Andrii Uhryn <andrii.uhryn@absa.africa>
2023-12-04 11:30:20 +0100

    fix: part of real registry data

--- packages/frontend/src/fixtures/stubs.ts
+++ packages/frontend/src/fixtures/stubs.ts
@@ -9,22 +9,11 @@ export const trustRegistryStub = {
       role: ['issuer'],
       credentials: [
         'did:indy:sovrin:staging:C279iyCR8wtKiPC8o9iPmb/anoncreds/v0/SCHEMA/e-KYC/1.0.0',
+        'did:indy:sovrin:staging:C279iyCR8wtKiPC8o9iPmb/anoncreds/v0/SCHEMA/e-KYC-mobile/1.0.0',
+        'did:indy:sovrin:staging:C279iyCR8wtKiPC8o9iPmb/anoncreds/v0/SCHEMA/e-KYC-residence/7.0.0',
+        'did:indy:sovrin:staging:C279iyCR8wtKiPC8o9iPmb/anoncreds/v0/SCHEMA/test-absa-bank-account/1.0.0',
       ],
       id: '8fa665b6-7fc5-4b0b-baee-6221b1844ec8',
-      createdAt: '2023-11-10T14:43:36.083Z',
-      updatedAt: '2023-11-20T16:49:58.307Z',
-    },
-    {
-      name: 'Example entity',
-      dids: ['did:indy:sovrin:staging:Pmi5CXJbv4HM8YttyomMoW'],
-      logo_url:
-        'https://s3.eu-central-1.amazonaws.com/builds.eth.company/absa.svg',
-      domain: 'www.example.com',
-      role: ['issuer'],
-      credentials: [
-        'did:indy:sovrin:staging:Pmi5CXJbv4HM8YttyomMoW/anoncreds/v0/SCHEMA/e-KYC/1.0.0',
-      ],
-      id: '6na665b6-7fc5-4b0b-baee-6221b1844ea7',
       createdAt: '2023-11-10T14:43:36.083Z',
       updatedAt: '2023-11-20T16:49:58.307Z',
     },
@@ -39,17 +28,31 @@ export const trustRegistryStub = {
     },
     {
       schemaId:
-        'did:indy:sovrin:staging:Pmi5CXJbv4HM8YttyomMoW/anoncreds/v0/SCHEMA/e-KYC/1.0.0',
-      name: 'Digital Identity 2',
-      createdAt: '2023-11-10T14:41:49.143Z',
-      updatedAt: '2023-11-20T16:49:09.838Z',
+        'did:indy:sovrin:staging:C279iyCR8wtKiPC8o9iPmb/anoncreds/v0/SCHEMA/e-KYC-mobile/1.0.0',
+      name: 'Phone Number',
+      createdAt: '2023-12-10T14:41:49.143Z',
+      updatedAt: '2023-12-20T16:49:09.838Z',
+    },
+    {
+      schemaId:
+        'did:indy:sovrin:staging:C279iyCR8wtKiPC8o9iPmb/anoncreds/v0/SCHEMA/e-KYC-residence/7.0.0',
+      name: 'Digital Identity',
+      createdAt: '2023-12-11T14:41:49.143Z',
+      updatedAt: '2023-12-21T16:49:09.838Z',
+    },
+    {
+      schemaId:
+        'did:indy:sovrin:staging:C279iyCR8wtKiPC8o9iPmb/anoncreds/v0/SCHEMA/test-absa-bank-account/1.0.0',
+      name: 'Bank Account',
+      createdAt: '2023-10-11T14:41:49.143Z',
+      updatedAt: '2023-10-21T16:49:09.838Z',
     },
   ],
 }
 
 export const submissionsStub = [
   {
-    name: 'John Doe',
+    name: 'Absa',
     dids: ['did:key:z6MkjBWPPa1njEKygyr3LR3pRKkqv714vyTkfnUdP6ToFSH5'],
     logo_url:
       'https://s3.eu-central-1.amazonaws.com/builds.eth.company/absa.svg',
@@ -64,69 +67,13 @@ export const submissionsStub = [
     updatedAt: '2023-11-16T15:24:31.630Z',
     state: 'pending',
   },
-  {
-    name: 'Charlotte Doe',
-    dids: ['did:key:z6MknhhUUtbXCLRmUVhYG7LPPWN4CTKWXTLsygHMD6Ah5uDN'],
-    logo_url:
-      'https://s3.eu-central-1.amazonaws.com/builds.eth.company/absa.svg',
-    domain: 'http://absa.co.za',
-    role: ['issuer'],
-    credentials: [
-      'did:indy:sovrin:staging:C279iyCR8wtKiPC8o9iPmb/anoncreds/v0/SCHEMA/e-KYC/1.0.0',
-    ],
-    invitationId: 'k4o74jb1u6bto0owh9si0sux',
-    id: '6a89a0fc-3e22-44d8-9d27-35c79b9f76c5',
-    createdAt: '2023-11-16T15:39:09.694Z',
-    updatedAt: '2023-11-16T15:39:09.694Z',
-    state: 'rejected',
-  },
-  {
-    name: 'Michael Doe',
-    dids: ['did:key:z6MknhhUUtbXCLRmUVhYG7LPPWN4CTKWXTLsygHMD6Ah5uDN'],
-    logo_url:
-      'https://s3.eu-central-1.amazonaws.com/builds.eth.company/absa.svg',
-    domain: 'http://absa.co.za',
-    role: ['issuer'],
-    credentials: [
-      'did:indy:sovrin:staging:C279iyCR8wtKiPC8o9iPmb/anoncreds/v0/SCHEMA/e-KYC/1.0.0',
-    ],
-    invitationId: 'k4o74jb1u6bto0owh9si0jgs',
-    id: '5a89a0fc-3e22-44d8-9d27-35c79b9f76c3',
-    createdAt: '2023-11-16T15:39:09.694Z',
-    updatedAt: '2023-11-16T15:39:09.694Z',
-    state: 'approved',
-  },
 ]
 
 export const invitationsStub = [
   {
-    emailAddress: 'john.doe@example.com',
+    emailAddress: 'admin@absa.africa',
     id: 's3gdwtyxt8w8dn87sbisxjn0',
     createdAt: '2023-11-16T14:50:02.207Z',
     entityId: '8fa665b6-7fc5-4b0b-baee-6221b1844ec8',
-  },
-  {
-    emailAddress: 'charlotte.doe@example.com',
-    id: 'k4o74jb1u6bto0owh9si0sux',
-    createdAt: '2023-11-16T15:15:53.438Z',
-    entityId: '6na665b6-7fc5-4b0b-baee-6221b1844ea7',
-  },
-  {
-    emailAddress: 'michael.doe@example.com',
-    id: 'k4o74jb1u6bto0owh9si0jgs',
-    createdAt: '2023-11-16T15:25:10.670Z',
-    entityId: '6na665b6-7fc5-4b0b-baee-6221b184h8d0',
-  },
-  {
-    emailAddress: 'alice.doe@example.com',
-    id: 'ib6o9ki9fssprzljkuth6fvj',
-    createdAt: '2023-11-16T15:26:00.362Z',
-    entityId: '6na665b6-7fc5-4b0b-baee-6221b184v6k3',
-  },
-  {
-    emailAddress: 'tom.doe@example.com',
-    id: 'f7vahm7zfx5sw24rb8i589yj',
-    createdAt: '2023-11-16T15:28:26.858Z',
-    entityId: '6na665b6-7fc5-4b0b-baee-6221b184lo8D',
   },
 ]
