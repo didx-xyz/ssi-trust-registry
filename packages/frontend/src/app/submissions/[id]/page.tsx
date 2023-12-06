153eded96cd35491b4bbadf687b708f9ebce92ef
Andrii Uhryn <andrii.uhryn@absa.africa>
2023-12-06 10:48:28 +0100

    fix: remove empty line

--- packages/frontend/src/app/submissions/[id]/page.tsx
+++ packages/frontend/src/app/submissions/[id]/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
-
 import { backendUrl, betterFetch } from '@/api'
 import { Submission } from '@/common/interfaces'
 import { NavigationBreadcrumbs } from '@/common/components/navigation/Breadcrumbs'
