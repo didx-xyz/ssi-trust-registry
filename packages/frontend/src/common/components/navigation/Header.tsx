ccf1c6f9e0932f348597e38cd22a6afb31e06c96
Andrii Uhryn <andrii.uhryn@absa.africa>
2023-12-08 09:18:29 +0100

    refactor: misspelling

--- packages/frontend/src/common/components/navigation/Header.tsx
+++ packages/frontend/src/common/components/navigation/Header.tsx
@@ -11,7 +11,7 @@ interface User {
   id?: string
 }
 
-const protectedPathes = ['/submissions', '/invitations']
+const protectedPaths = ['/submissions', '/invitations']
 
 export function Header() {
   const currentPathname = usePathname()
@@ -114,5 +114,5 @@ async function getPendingSubmissionsCount(): Promise<number> {
 }
 
 function isPageProtected(pathname: string) {
-  return !!protectedPathes.find((path) => pathname.includes(path))
+  return !!protectedPaths.find((path) => pathname.includes(path))
 }
