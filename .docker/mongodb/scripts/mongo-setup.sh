341ae688717738ddd14af38285e771a71d94b4fe
Niall Shaw <niall.shaw@absa.africa>
2024-01-17 18:50:02 +0800

    chore: rename mongodb js files

--- .docker/mongodb/scripts/mongo-setup.sh
+++ .docker/mongodb/scripts/mongo-setup.sh
@@ -3,5 +3,5 @@
 echo "Waiting for startup.."
 sleep 5
 echo mongosetup.sh time now: $(date +"%T")
-mongosh --host mongo:27017 -u ${MONGO_INITDB_ROOT_USERNAME} -p ${MONGO_INITDB_ROOT_PASSWORD} < $(dirname "$0")/rs-init.js
+mongosh --host mongo:27017 -u ${MONGO_INITDB_ROOT_USERNAME} -p ${MONGO_INITDB_ROOT_PASSWORD} < $(dirname "$0")/init-replica-set.js
 echo "done"
